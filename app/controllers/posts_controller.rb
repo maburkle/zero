030958cf82a42fd096b76fd1b7bc624236203d06
Mike Burkle <Michael.burkle@lvlsvn.com>
2013-09-19 14:27:21 -0400

    Some style changes in forum page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,7 +32,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { render 'show' }
+        format.html { redirect_to forum_subject_topic_discuss_posts_path(discuss_id: @discuss) }
         format.json { render action: 'show', status: :created, location: @post }
       else
         format.html { render action: 'new' }
