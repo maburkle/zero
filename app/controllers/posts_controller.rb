b4cd9a472491b32ca5106c3e6a37a16c59f6cad7
Mike Burkle <Michael.burkle@lvlsvn.com>
2013-09-24 12:58:34 -0400

    added timestamps

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,7 +69,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = @discuss.posts.find(params[:discuss_id])
+      @post = @discuss.posts.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
