3c6a3f81dc00935946ab7ac113a15706c3309176
Mike Burkle <Michael.burkle@lvlsvn.com>
2013-11-14 13:51:36 -0500

    home page changes 8

--- db/migrate/20131114162416_removeemailindex.rb
+++ db/migrate/20131114162416_removeemailindex.rb
@@ -1,5 +1,13 @@
 class Removeemailindex < ActiveRecord::Migration
   def change
   	remove_index(:users, :name => 'index_users_on_email')
+    create_table :rsvps do |t|
+
+    	t.integer :event_id
+    	t.integer :user_id
+    	t.string :main_job
+    	t.string :second_job
+      t.timestamps
+    end
   end
 end
