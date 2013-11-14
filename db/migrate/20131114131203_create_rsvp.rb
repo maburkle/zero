3a7659cdc5f01047fe9a388a76b67b904ee91123
Mike Burkle <Michael.burkle@lvlsvn.com>
2013-11-14 13:57:12 -0500

    home page changes 9

--- db/migrate/20131114131203_create_rsvp.rb
+++ db/migrate/20131114131203_create_rsvp.rb
@@ -1,6 +1,5 @@
 class CreateRsvp < ActiveRecord::Migration
   def change
-    change_column :rsvps, :event_id, :integer
-    change_column :rsvps, :user_id, :integer
+  	drop_table :rsvps
   end
 end
