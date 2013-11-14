0f0ea3fbae84d68c44c08feb059cf2064d23a3c5
Mike Burkle <Michael.burkle@lvlsvn.com>
2013-11-14 14:57:05 -0500

    clean up Sign In

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Zero
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << "#{Rails.root}/vendor/assets/bootstrap"
-    config.assets.compile = true
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
